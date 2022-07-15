7c6177aee595efc792aa3e90256dadf7d96639a0
dokoye98 <dwayneokoye98@gmail.com>
2022-07-15 20:29:08 +0000

    tenno dumy data

--- create.py
+++ create.py
@@ -4,5 +4,10 @@ from application.models import Customer,Tenno
 db.drop_all()
 db.create_all()
 custom1=Customer(first_name='qda',last_name='1231',username='1231',password='sdasa',card_details='1231',membership=False)
+tenno1=Tenno(warframe='rhino',
+            health=123,
+            mp=1231,
+            prime=True)
 db.session.add(custom1)
+db.session.add(tenno1)
 db.session.commit()
